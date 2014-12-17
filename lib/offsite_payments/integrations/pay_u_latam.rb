61404330e9775a25a1be85f207e8854c671ca834
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-12-17 05:46:21 -0800

    pay_u_latam: migrate from PagosOnline to PayULatam API
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/offsite_payments/integrations/pay_u_latam.rb
+++ lib/offsite_payments/integrations/pay_u_latam.rb
@@ -5,10 +5,19 @@ module OffsitePayments #:nodoc:
     module PayULatam
 
       mattr_accessor :test_url
-      self.test_url = 'https://gateway2.pagosonline.net/ws/WebServicesClientesUT'
+      self.test_url = 'https://stg.api.payulatam.com/payments-api/4.0/service.cgi'
 
       mattr_accessor :production_url
-      self.production_url = 'https://gateway2.pagosonline.net/ws/WebServicesClientesUT'
+      self.production_url = 'https://api.payulatam.com/payments-api/4.0/service.cgi'
+
+      QUERIES_API_TEST_URL = 'https://stg.api.payulatam.com/reports-api/4.0/service.cgi'
+      QUERIES_API_LIVE_URL = 'https://api.payulatam.com/reports-api/4.0/service.cgi'
+
+      QUERY_COMMANDS = {
+          :orderId       => 'ORDER_DETAIL',
+          :referenceCode => 'ORDER_DETAIL_BY_REFERENCE_CODE',
+          :transactionId => 'TRANSACTION_RESPONSE_DETAIL',
+      }
 
       def self.service_url
         mode = OffsitePayments.mode
@@ -22,241 +31,214 @@ module OffsitePayments #:nodoc:
         end
       end
 
-      def self.notification(post)
-        Notification.new(post)
+      def self.queries_url
+        mode = OffsitePayments.mode
+        case mode
+          when :production
+            QUERIES_API_LIVE_URL
+          when :test
+            QUERIES_API_TEST_URL
+          else
+            raise StandardError, "Integration mode set to an invalid value: #{mode}"
+        end
+      end
+
+      def self.helper(order, account, options = {})
+        Helper.new(order, account, options)
       end
 
       class Helper < OffsitePayments::Helper
-        # Replace with the real mapping
-        mapping :account, ''
-        mapping :amount, ''
 
-        mapping :order, ''
+        mapping :customer,
+                :name       => 'name',
+                :email      => 'email',
+                :phone      => 'phone',
+                :dni_number => 'dniNumber'
 
-        mapping :customer, :first_name => '',
-                :last_name             => '',
-                :email                 => '',
-                :phone                 => ''
+        mapping :billing_address,
+                :address1 => 'address1',
+                :city     => 'city',
+                :state    => 'state',
+                :country  => 'country'
 
-        mapping :billing_address, :city => '',
-                :address1               => '',
-                :address2               => '',
-                :state                  => '',
-                :zip                    => '',
-                :country                => ''
+        mapping :amount, 'amount'
+        mapping :currency, 'currency'
+        mapping :order, 'order'
+        mapping :description, 'description'
 
-        mapping :notify_url, ''
-        mapping :return_url, ''
-        mapping :cancel_return_url, ''
-        mapping :description, ''
-        mapping :tax, ''
-        mapping :shipping, ''
+        mapping :language, 'language'
+        mapping :payment_method, 'paymentMethod'
+        mapping :expiration_date, 'expirationDate'
 
-        # All credentials are mandatory and need to be set
+        # These credentials are mandatory:
         #
-        # credential2: usuario_id
-        # credential3: cuenta_id
-        # credential4: token password
-        def initialize(order, account, options = {})
+        # credential2: api_login
+        # credential3: api_key
+        # credential4: account_id
+        def initialize(order, merchant_id, options = {})
           super
-          @options = options
-       end
+          @merchant_id = merchant_id
+          @options     = options
 
-        ENVELOPE_NAMESPACES = {
-            'xmlns:xsd'  => 'http://www.w3.org/2001/XMLSchema',
-            'xmlns:xsi'  => 'http://www.w3.org/2001/XMLSchema-instance',
-            'xmlns:impl' => 'https://gateway2.pagosonline.net/ws/WebServicesClientesUT',
-            'xmlns:env'  => 'http://schemas.xmlsoap.org/soap/envelope/',
-            'xmlns:ins0' => 'http://axis.web.v2.pagosonline.net',
-            'xmlns:ins1' => 'urn:Pagosonline',
-            'xmlns:ins2' => 'http://common.v2.pagosonline.net',
-            'xmlns:ins3' => 'http://xml.apache.org/xml-soap',
-            'xmlns:ins4' => 'http://client.v2.pagosonline.net',
-            'xmlns:ins5' => 'http://controllers.web.v2.pagosonline.net'
-        }
+          add_field 'language', 'en'
+        end
 
-        def usuario_id
+        def api_login
           @options[:credential2]
         end
 
-        def cuenta_id
+        def api_key
           @options[:credential3]
         end
 
-        def token_id
-          usuario_id
-        end
-
-        def token_password
+        def account_id
           @options[:credential4]
         end
 
-        def initiate_pse_transaction(money, options)
-          body = build_request(build_initiate_pse_transaction_request(money, options))
-          do_request(PayULatam.service_url, body)
+        def merchant_id
+          @merchant_id
+        end
+
+        def form_method
+          'GET'
+        end
+
+        def form_fields
+          cash_request = send_request(PayULatam.service_url, build_cash_request)
+
+          raise ActionViewHelperError, "Invalid response: #{cash_request}" unless success_cash_request_response?(cash_request)
+
+          {
+              :order_id        => cash_request['transactionResponse']['orderId'],
+              :transaction_id  => cash_request['transactionResponse']['transactionId'],
+              :url             => cash_request['transactionResponse']['extraParameters']['URL_PAYMENT_RECEIPT_HTML'],
+              :expiration_date => cash_request['transactionResponse']['extraParameters']['EXPIRATION_DATE'],
+          }
+        end
+
+        def order_status(order_id)
+          request_status = send_request(PayULatam.queries_url, build_query_request(:orderId, order_id))
+
+          raise ActionViewHelperError, "Invalid response: #{request_status}" unless success_response?(request_status)
+          # Not found?
+          return nil if request_status['result'].nil? or request_status['result']['payload'].nil?
+
+          {
+              :order_id       => request_status['result']['payload']['id'],
+              :status         => request_status['result']['payload']['status'],
+              :reference_code => request_status['result']['payload']['referenceCode'],
+              :description    => request_status['result']['payload']['description']
+          }
+        end
+
+        def transaction_status(transaction_id)
+          request_status = send_request(PayULatam.queries_url, build_query_request(:transactionId, transaction_id))
+
+          raise ActionViewHelperError, "Invalid response: #{request_status}" unless success_response?(request_status)
+          # Not found?
+          return nil if request_status['result'].nil? or request_status['result']['payload'].nil?
+
+          {
+              :status            => request_status['result']['payload']['state'],
+              :traceability_code => request_status['result']['payload']['trazabilityCode'],
+              :authorization     => request_status['result']['payload']['authorizationCode']
+          }
+        end
+
+        def orders_statuses(reference_code)
+          request_status = send_request(PayULatam.queries_url, build_query_request(:referenceCode, reference_code))
+
+          raise ActionViewHelperError, "Invalid response: #{request_status}" unless success_response?(request_status)
+          # Not found?
+          return nil if request_status['result'].nil?
+
+          request_status['result']['payload']
         end
 
         private
 
-        def build_initiate_pse_transaction_request(money, options = {})
-          xml = Builder::XmlMarkup.new
-
-          xml.tag! 'impl:iniciarTransaccionPseConInformacionAdicional' do
-            xml.tag! 'usuarioId', usuario_id
-            xml.tag! 'cuentaId', cuenta_id
-            xml.tag! 'referencia', options[:order_id]
-            xml.tag! 'descripcion', options[:description]
-            xml.tag! 'valor', (money / 100).to_i
-            xml.tag! 'iva', options[:tax]
-            xml.tag! 'emailComprador', options[:email]
-            xml.tag! 'direccionIpComprador', options[:ip]
-            xml.tag! 'nombreComprador', options[:name]
-            xml.tag! 'telefonoComprador', options[:phone]
-            xml.tag! 'cookie', options[:cookie]
-            xml.tag! 'agenteNavegador', options[:user_agent]
-            xml.tag! 'urlRespuesta', options[:success_url]
-            xml.tag! 'urlConfirmacion', options[:confirmation_url]
-            xml.tag! 'codigoBancoPse', options[:bank_code]
-            xml.tag! 'tipoCliente', 'N'
-            xml.tag! 'pseReferencia1', options[:ip]
-            xml.tag! 'pseReferencia2', 'CC'
-            xml.tag! 'pseReferencia3', options[:document_id]
-            # TODO
-            xml.tag! 'extra1', signature(nil, options)
-          end
-
-          xml.target!
-        end
-
-        def signature(salt, options)
-          raw = "#{options[:email]}~^~#{options[:ip]}CC#{options[:document_id]}#{salt}"
-          Digest::SHA256.hexdigest(raw)[0..32]
-        end
-
-        def build_request(body)
-          xml = Builder::XmlMarkup.new
-
-          xml.instruct!
-          xml.tag! 'env:Envelope', ENVELOPE_NAMESPACES do
-            xml.tag! 'env:Header' do
-              xml.tag! 'wsse:Security', {'env:mustUnderstand' => '1', 'xmlns:wsse' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'} do
-                xml.tag! 'wsse:UsernameToken' do
-                  xml.tag! 'wsse:Username', token_id
-                  xml.tag! 'wsse:Password', token_password, 'Type' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText'
-                end
-              end
-            end
-            xml.tag! 'env:Body', {'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema'} do
-              xml << body
-            end
-          end
-          xml.target!
-        end
-
-        def do_request(url, post_body = nil)
-          uri = URI.parse(url)
-
-          http         = Net::HTTP.new(uri.host, uri.port)
-          http.use_ssl = true
-
-          headers      = {'SOAPAction'   => url,
-                          'Content-Type' => 'text/xml; charset=utf-8'}
-          request      = Net::HTTP::Post.new(uri.request_uri, headers)
-          request.body = post_body
-
-          http.request(request).body
-        end
-      end
-
-      class Notification < OffsitePayments::Notification
-        def complete?
-          params['']
-        end
-
-        def item_id
-          params['']
-        end
-
-        def transaction_id
-          params['']
-        end
-
-        # When was this payment received by the client.
-        def received_at
-          params['']
-        end
-
-        def payer_email
-          params['']
-        end
-
-        def receiver_email
-          params['']
-        end
-
-        def security_key
-          params['']
-        end
-
-        # the money amount we received in X.2 decimal.
-        def gross
-          params['']
-        end
-
-        # Was this a test transaction?
-        def test?
-          params[''] == 'test'
-        end
-
-        def status
-          params['']
-        end
-
-        # Acknowledge the transaction to PayULatam. This method has to be called after a new
-        # apc arrives. PayULatam will verify that all the information we received are correct and will return a
-        # ok or a fail.
-        #
-        # Example:
-        #
-        #   def ipn
-        #     notify = PayULatamNotification.new(request.raw_post)
-        #
-        #     if notify.acknowledge
-        #       ... process order ... if notify.complete?
-        #     else
-        #       ... log possible hacking attempt ...
-        #     end
-        def acknowledge(authcode = nil)
-          payload = raw
-
-          uri = URI.parse(PayULatam.notification_confirmation_url)
-
-          request = Net::HTTP::Post.new(uri.path)
-
-          request['Content-Length'] = "#{payload.size}"
-          request['User-Agent']     = 'Active Merchant -- http://activemerchant.org/'
-          request['Content-Type']   = 'application/x-www-form-urlencoded'
-
+        def send_request(url, request_body)
+          uri              = URI.parse(url)
           http             = Net::HTTP.new(uri.host, uri.port)
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @ssl_strict
           http.use_ssl     = true
+          # PayULatam's test server has an improperly installed cert
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE if test?
 
-          response = http.request(request, payload)
+          request              = Net::HTTP::Post.new(uri.request_uri)
+          request['Accept']    = 'application/json'
+          request.content_type = 'application/json'
+          request.body         = request_body
 
-          # Replace with the appropriate codes
-          raise StandardError.new("Faulty PayULatam result: #{response.body}") unless ['AUTHORISED', 'DECLINED'].include?(response.body)
-          response.body == 'AUTHORISED'
+          response = http.request(request)
+          JSON.parse(response.body)
+        rescue JSON::ParserError
+          response.body
         end
 
-        private
+        def build_cash_request
+          shipping_address           = {}
+          shipping_address[:street1] = @fields['address1'] unless @fields['address1'].nil?
+          shipping_address[:city]    = @fields['city'] unless @fields['city'].nil?
+          shipping_address[:state]   = @fields['state'] unless @fields['state'].nil?
+          shipping_address[:country] = @fields['country'] unless @fields['country'].nil?
+          shipping_address[:phone]   = @fields['phone'] unless @fields['phone'].nil?
 
-        # Take the posted data and move the relevant data into a hash
-        def parse(post)
-          @raw = post.to_s
-          for line in @raw.split('&')
-            key, value  = *line.scan(%r{^([A-Za-z0-9_.-]+)\=(.*)$}).flatten
-            params[key] = CGI.unescape(value.to_s) if key.present?
-          end
+          buyer                   = {}
+          buyer[:fullName]        = @fields['name'] unless @fields['name'].nil?
+          buyer[:emailAddress]    = @fields['email'] unless @fields['email'].nil?
+          buyer[:dniNumber]       = @fields['dniNumber'] unless @fields['dniNumber'].nil?
+          buyer[:shippingAddress] = shipping_address
+
+          order                    = {}
+          order[:accountId]        = account_id
+          order[:referenceCode]    = @fields['order']
+          order[:description]      = @fields['description']
+          order[:language]         = @fields['language']
+          order[:signature]        = signature
+          order[:shippingAddress]  = {:country => @fields['country']} unless @fields['country'].nil?
+          order[:buyer]            = buyer
+          order[:additionalValues] = {:TX_VALUE => {:value => @fields['amount'].to_i, :currency => @fields['currency']}}
+
+          transaction                  = {}
+          transaction[:order]          = order
+          transaction[:type]           = 'AUTHORIZATION_AND_CAPTURE'
+          transaction[:paymentMethod]  = @fields['paymentMethod']
+          transaction[:expirationDate] = @fields['expirationDate'] unless @fields['expirationDate'].nil?
+
+          request               = build_base_request('SUBMIT_TRANSACTION')
+          request[:transaction] = transaction
+
+          request.to_json
+        end
+
+        def build_query_request(key, value)
+          request           = build_base_request(QUERY_COMMANDS[key])
+          request[:details] = {key => value}
+
+          request.to_json
+        end
+
+        def build_base_request(command)
+          request            = {}
+          request[:language] = @fields['language']
+          request[:command]  = command
+          # Should always be false, even in test mode
+          request[:test]     = 'false'
+          request[:merchant] = {:apiLogin => api_login, :apiKey => api_key}
+          request
+        end
+
+        def signature
+          raw = "#{api_key}~#{merchant_id}~#{@fields['order']}~#{@fields['amount']}~#{@fields['currency']}"
+          Digest::MD5.hexdigest(raw)
+        end
+
+        def success_cash_request_response?(cash_request_response)
+          success_response?(cash_request_response) and cash_request_response['transactionResponse'].is_a?(Hash) and cash_request_response['transactionResponse']['errorCode'].nil?
+        end
+
+        def success_response?(response)
+          response.is_a?(Hash) and response['code'] == 'SUCCESS' and response['error'].nil?
         end
       end
     end
